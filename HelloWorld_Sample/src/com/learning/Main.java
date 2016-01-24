94c3b736f41b2bbd14c261e516d1c27ce76e7ae8
Alston <and_croning@hotmail.com>
2016-01-24 01:19:09 -0500

    joy_world feature updated

--- HelloWorld_Sample/src/com/learning/Main.java
+++ HelloWorld_Sample/src/com/learning/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("Big World is truly big!");
+        System.out.println("Joy   World is truly big!");
+        System.out.println("It make the World joyful!");
+        System.out.println("So we should be joyful about it");
     }
 }
