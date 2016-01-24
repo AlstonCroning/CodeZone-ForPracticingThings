eded9f3a0ba91dfa9ec4248ab8475ad922867d2f
Alston <and_croning@hotmail.com>
2016-01-24 01:12:54 -0500

    happy_world feature updated

--- HelloWorld_Sample/src/com/learning/Main.java
+++ HelloWorld_Sample/src/com/learning/Main.java
@@ -3,7 +3,8 @@ package com.learning;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-        System.out.println("Big World is truly big!");
+         // write your code here
+        System.out.println("Happy World is truly big!");
+       System.out.println("It makes a Happy World");
     }
 }
