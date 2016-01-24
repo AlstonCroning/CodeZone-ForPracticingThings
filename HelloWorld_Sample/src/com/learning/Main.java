4c344e3febdd2de448ac3c00b7b4f394120543bd
Alston <and_croning@hotmail.com>
2016-01-24 00:43:06 -0500

    Big World Feature being updated

--- HelloWorld_Sample/src/com/learning/Main.java
+++ HelloWorld_Sample/src/com/learning/Main.java
@@ -3,7 +3,7 @@ package com.learning;
 public class Main {
 
     public static void main(String[] args) {
-	    // write your code here
-        System.out.println("Big World");
+        // write your code here
+        System.out.println("Big World is truly big!");
     }
 }
