c0cf1565228163a6976f3c22023c97fa6e4ed20f
Alston <and_croning@hotmail.com>
2016-02-05 00:57:13 -0500

    Learning Phalcon framework with a sample project called sharecomments

---
+++ ShareComments_Using_Phalcon/app/controllers/IndexController.php
@@ -0,0 +1,11 @@
+<?php
+
+use phalcon\Mvc\Controller;
+
+class IndexController extends Controller
+{
+    public function indexAction()
+    {
+
+    }
+}
---
+++ ShareComments_Using_Phalcon/public/js/script.js
@@ -0,0 +1,4 @@
+document.write("This is Java Script");
+
+var msg = document.getElementById('message');
+msg.innerHTML += "This is Java Script Again";
