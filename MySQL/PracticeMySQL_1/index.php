4eb766a119003841a0aa60be86718d77adae646a
Alston <and_croning@hotmail.com>
2016-03-16 00:36:46 -0400

    several SQL statements used for the demonstration

--- MySQL/PracticeMySQL_1/index.php
+++ MySQL/PracticeMySQL_1/index.php
@@ -1,2 +1,34 @@
 <?php
 
+//connection to the database
+$link = mysqli_connect("localhost","root","a1xcr0nge","sampledatabase");
+
+if(mysqli_connect_error()){
+    die("Couldn't connect to database");
+}
+
+/*
+//default SQL statement
+//$query = "SELECT * FROM `users`";
+*/
+
+/*
+//extracting data from the sampledatabase
+$query = "SELECT * FROM `users` WHERE `name` LIKE '%th%'";
+*/
+
+/*
+//updating some data
+$query = "UPDATE `users` SET `email`='bethIO@gmail.com' WHERE `id`='7' LIMIT 1";
+mysqli_query($link,$query);
+*/
+
+
+if($result = mysqli_query($link,$query)){
+    while($row = mysqli_fetch_array($result)) {
+        print_r($row);
+    }
+}
+
+
+?>
