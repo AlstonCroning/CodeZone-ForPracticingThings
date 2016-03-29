5c556a2d8d77c343b19fda23e68a01f7389024af
Alston <and_croning@hotmail.com>
2016-03-29 00:00:41 -0400

    some success with ajax, and json response. To be continued with more improvements.

--- SampleProjects/EndPoints/BackendPHP.php
+++ SampleProjects/EndPoints/BackendPHP.php
@@ -1,1 +1,32 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+$link = mysqli_connect("localhost","root","a1xcr0nge","sampledatabase");
+if(mysqli_connect_error()) {
+//    echo "Couldn't connect to Database";
+    die("Couldn't connect to DataBase");
+}
+
+if($_SERVER['REQUEST_METHOD'] == "POST") {
+
+    $comment = $_POST['inputComment1'];
+    $username = $_POST['username'];
+
+    if ($comment != "" && $username != "") {
+        $query = "INSERT INTO `users` (`name`,`comments`) VALUES ('$username','$comment')";
+        mysqli_query($link, $query);
+
+
+        $query = "SELECT * FROM `users` WHERE `name`='$username'";
+
+        if ($result = mysqli_query($link, $query)) {
+            while ($rows = mysqli_fetch_array($result)) {
+                print_r($rows);
+            }
+        }
+
+        //print $comment;
+    }
+}
--- SampleProjects/EndPoints/JavaScript.js
+++ SampleProjects/EndPoints/JavaScript.js
@@ -1,1 +1,15 @@
-document.write("hello world");
+//sample Ajax Demonstration
+var button = document.getElementById("btn");
+button.addEventListener("click",loadDoc);
+
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if(xhttp.readyState == 4 && xhttp.status == 200) {
+            document.getElementById("message").innerHTML = xhttp.responseText;
+        }
+    };
+    xhttp.open("POST","BackendPHP.php",true);
+    xhttp.send();
+}
+
