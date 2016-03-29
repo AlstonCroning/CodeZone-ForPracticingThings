f1bf4cc7d62768ea66039bd3da899fed174415d4
Alston <and_croning@hotmail.com>
2016-03-29 05:17:41 -0400

    working jason response and Ajax. to be continued..

--- WarmupPractices/PhpWithJson_1/backend.php
+++ WarmupPractices/PhpWithJson_1/backend.php
@@ -1,1 +1,38 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+
+if($_SERVER['REQUEST_METHOD'] == "POST") {
+
+$firstName = $_POST['fname'];
+$lastName = $_POST['lname'];
+
+
+    $output = "[";
+    $output .= '{"firstName":"'.$firstName.'"},';
+    $output .= '{"lastName":"'.$lastName.'"}';
+    $output .= "]";
+    echo $output;
+
+    //$output .= '{"Name": "Dishan"}';
+
+//Json Response
+/*    $output = "[";
+    $output .= '{"firstName":   '.$firstName.'","';
+    $output .= '"lastName":    '.$lastName.'"}"';
+    $output .= "]";
+    echo $output;*/
+
+/* //working code
+    $output = "[";
+    $output .= '{"Name": "Dishan"}';
+    $output .= "]";
+    echo $output;
+*/
+
+
+
+
+}
--- WarmupPractices/PhpWithJson_1/javaScript.js
+++ WarmupPractices/PhpWithJson_1/javaScript.js
@@ -1,1 +1,38 @@
-document.write("Hello World Test");
+var output = document.getElementById("output");
+var button = document.getElementById("btn");
+
+var firstName = document.getElementById("fname");
+var lastName = document.getElementById("lname");
+
+button.addEventListener("click",loadDoc);
+
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if(xhttp.readyState == 4 && xhttp.status == 200) {
+            //output.innerHTML = xhttp.responseText;
+            //xhttp.responseText;
+            //myFunction(xhttp.responseText);
+
+            var arr = JSON.parse(xhttp.responseText);
+            myFunction(arr);
+
+
+        }
+    };
+    xhttp.open("POST","backend.php",true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("fname="+firstName.value+"&lname="+lastName.value);
+}
+
+function myFunction(response) {
+    var out = "";
+    var _firstName = "";
+    var _lastName = "";
+    for(i=0; i < response.length; i++) {
+        _firstName += response[i].firstName;
+        _lastName += response[i].lastName;
+        //out += "First Name: " + response[i].firstName;
+    }
+    output.innerHTML = "First Name: " + _firstName + "<br/>Last Name: " + _lastName;
+}
