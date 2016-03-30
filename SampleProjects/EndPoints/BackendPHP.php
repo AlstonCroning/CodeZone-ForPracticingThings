7a34b9b539d8849aab1a3fc795c5f5d48931cc33
Alston <and_croning@hotmail.com>
2016-03-30 03:23:06 -0400

    ajax code fixed

--- SampleProjects/EndPoints/BackendPHP.php
+++ SampleProjects/EndPoints/BackendPHP.php
@@ -1,32 +1,32 @@
 <?php
 
+//headers
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
+//connection to database
 $link = mysqli_connect("localhost","root","a1xcr0nge","sampledatabase");
 if(mysqli_connect_error()) {
-//    echo "Couldn't connect to Database";
     die("Couldn't connect to DataBase");
 }
 
+//Data Interaction & Response
 if($_SERVER['REQUEST_METHOD'] == "POST") {
 
-    $comment = $_POST['inputComment1'];
+    $comment = $_POST['inputComment'];
     $username = $_POST['username'];
 
     if ($comment != "" && $username != "") {
+        //inserting data into database
         $query = "INSERT INTO `users` (`name`,`comments`) VALUES ('$username','$comment')";
         mysqli_query($link, $query);
 
-
+        //selecting data from the database
         $query = "SELECT * FROM `users` WHERE `name`='$username'";
-
         if ($result = mysqli_query($link, $query)) {
             while ($rows = mysqli_fetch_array($result)) {
-                print_r($rows);
+                print_r($rows);//prints the data
             }
         }
-
-        //print $comment;
     }
 }
--- SampleProjects/EndPoints/JavaScript.js
+++ SampleProjects/EndPoints/JavaScript.js
@@ -1,15 +1,22 @@
 //sample Ajax Demonstration
 var button = document.getElementById("btn");
+var userName = document.getElementById("username");
+var userComment = document.getElementById("inputComment");
+var msg = document.getElementById("message");
+
+//event
 button.addEventListener("click",loadDoc);
 
 function loadDoc() {
+
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if(xhttp.readyState == 4 && xhttp.status == 200) {
-            document.getElementById("message").innerHTML = xhttp.responseText;
+            msg.innerHTML = xhttp.responseText;
         }
     };
     xhttp.open("POST","BackendPHP.php",true);
-    xhttp.send();
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("username="+userName.value+"&inputComment="+userComment.value);
 }
 
