9fc25b08c51463680b249e911273e7ba5c099afe
Alston <and_croning@hotmail.com>
2016-05-15 01:52:40 -0400

    completed PrintiNumbersWithA_Pattern exercise with OOP principles

---
+++ WarmupPractices/ChallengeTime_Exersice5_OOP/src/com/AlstonCroning/Main.java
@@ -0,0 +1,10 @@
+package com.AlstonCroning;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //exercise 5
+        PrintNumbersWithA_Pattern ob1 = new PrintNumbersWithA_Pattern();
+        ob1.printLikeNumbers();//display numbers
+    }
+}
