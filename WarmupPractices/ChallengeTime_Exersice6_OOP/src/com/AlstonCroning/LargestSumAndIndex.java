efe79de0207a1d815cfef699ed9bf8bc07877996
Alston <and_croning@hotmail.com>
2016-05-15 18:07:45 -0400

    partially completed exercise 6

--- WarmupPractices/ChallengeTime_Exersice6_OOP/src/com/AlstonCroning/LargestSumAndIndex.java
+++ WarmupPractices/ChallengeTime_Exersice6_OOP/src/com/AlstonCroning/LargestSumAndIndex.java
@@ -1,53 +1,21 @@
 package com.AlstonCroning;
 
 public class LargestSumAndIndex {
-    private int sum;
-    private int index;
-    private PossibleSolutions possibleSolutions;
-    private BestSolution bestSolution;
-    private StartingIndex startingIndex;
-    private int [] array;
-    private int groupSize;
-//    int [] largestSumAndIndex = new int[2];//used to store largestSum and starting index
-    private int [] pSolutions;
-    private int bSolution;
-    private int sIndex;
+    private int largestsum;
+    private int startingIndex;
 
-    public LargestSumAndIndex(int sum, int index) {
-        this.sum = sum;
-        this.index = index;
+    //constructor
+    public LargestSumAndIndex(int largestsum, int startingIndex) {
+        this.largestsum = largestsum;
+        this.startingIndex = startingIndex;
     }
-
-    public LargestSumAndIndex findTheLargestSumOfConsecutiveEntries(int[] array, int groupSize) {
-        this.array = array;
-        this.groupSize = groupSize;
-
-        pSolutions = getPossibleSolutions().possibleSolutions();
-        bSolution = getBestSolution().bestSolution(pSolutions);
-        sIndex = getStartingIndex().findStartingIndexOfBestSolution(pSolutions,bSolution);
-
-        return new LargestSumAndIndex(bSolution,sIndex);
-    }
-
 
     //getters
-    public PossibleSolutions getPossibleSolutions() {
-        return possibleSolutions;
+    public int getLargestsum() {
+        return largestsum;
     }
 
-    public BestSolution getBestSolution() {
-        return bestSolution;
-    }
-
-    public StartingIndex getStartingIndex() {
+    public int getStartingIndex() {
         return startingIndex;
-    }
-
-    public int getSum() {
-        return sum;
-    }
-
-    public int getIndex() {
-        return index;
     }
 }
