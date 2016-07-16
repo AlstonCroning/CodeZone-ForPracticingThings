945d035246cbfafcabaf6def07a6196094be0523
Alston <and_croning@hotmail.com>
2016-07-16 02:18:41 -0400

    adding missing code for (solved comboString coding exercise)

--- WarmupPractices/Coding Questions/comboStrings/src/com/AlstonCroning/Main.java
+++ WarmupPractices/Coding Questions/comboStrings/src/com/AlstonCroning/Main.java
@@ -3,6 +3,20 @@ package com.AlstonCroning;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String result = comboString("di","dis");
+        System.out.println(result);
+    }
+
+    //given 2 strings a and b. return a string of short+long+short
+    private static String comboString(String a,String b) {
+        int lengthA = a.length();
+        int lengthB = b.length();
+
+        //checking whether a is bigger
+        if(lengthA >= lengthB){
+            return (b+a+b);
+        } else {
+            return (a+b+a);
+        }
     }
 }
