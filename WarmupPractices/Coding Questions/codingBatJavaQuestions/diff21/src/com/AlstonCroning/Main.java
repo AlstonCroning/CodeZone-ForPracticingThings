912232c3967fc4dca7f456ea76d94782c33608a8
Alston <and_croning@hotmail.com>
2016-12-15 22:35:03 -0500

    showDiff Qn completed

---
+++ WarmupPractices/Coding Questions/codingBatJavaQuestions/diff21/src/com/AlstonCroning/Main.java
@@ -0,0 +1,19 @@
+package com.AlstonCroning;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static int diff21(int n) {
+
+        if(n >= 21) {
+            return 2*(n - 21);
+        }
+        return (21 - n);
+
+    }
+
+
+}
